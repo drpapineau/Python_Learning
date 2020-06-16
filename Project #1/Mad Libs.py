8dd027c965e3c075f5fa4cc0b5a8ffdf1124782b
drpapineau <vincent.courtois@netsatori.com>
2020-06-15 23:56:04 -0400

    done pour a soir on continue demain

---
+++ Project #1/Mad Libs.py
@@ -0,0 +1,13 @@
+
+Value1 = input("whats your name ")
+Value2 = input("Which enemy did you kill ")
+Value3 = input("whats your homeland ")
+Value4 = input("whats do you carry ")
+Value5 = input("which color is your horses ")
+
+
+
+print(f'\n\n Once upon a time a warrior named {Value1}\n' + f'W ell known in the land for the kill of the {Value2}' )
+
+
+
