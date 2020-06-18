e079d01f3c105e5a89fe1378532957408616e38e
drpapineau <vincent.courtois@netsatori.com>
2020-06-18 19:19:30 -0400

    done until later

---
+++ Bootcamp Project/02-Python-Statements/task-1.py
@@ -0,0 +1,16 @@
+#####    Use for, .split(), and if to create a Statement that will print out words that start with 's':
+####     st = 'Print only the words that start with s in this sentence'
+
+txt = "Print only the words that start with s in this sentence"
+word = txt.split()
+for x in word:
+    if x[0] == "s":
+        print (x)
+    
+          
+
+
+  
+        
+
+
---
+++ Bootcamp Project/02-Python-Statements/task-2.py
@@ -0,0 +1,9 @@
+## Use range() to print all the even numbers from 0 to 10.
+
+num = range(1, 11)
+
+#print(type(num))
+
+for x in num:
+    if (x%2) == 0:
+        print (x)
