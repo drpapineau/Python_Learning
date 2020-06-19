a686166e64dddb6c7ef4c85de0658e6d463ca7bc
drpapineau <vincent.courtois@netsatori.com>
2020-06-18 23:34:40 -0400

    done for tonight

--- Bootcamp Project/02-Python-Statements/task-3.py
+++ Bootcamp Project/02-Python-Statements/task-3.py
@@ -1,2 +1,7 @@
 ##     Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
 
+#  Numbers = range(1 , 11) aide de mathieu pour optimiser le code
+for x in range(1, 11):
+    if (x % 3) == 0:
+        print (x)
+        
---
+++ Bootcamp Project/02-Python-Statements/task-4.py
@@ -0,0 +1,4 @@
+###  Go through the string below and if the length of a word is even print "even!"
+###   st = 'Print every word in this sentence that has an even number of letters'
+
+
