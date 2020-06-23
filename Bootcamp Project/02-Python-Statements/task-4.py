989f7be41c00206171f95797b7c8ca31170b753b
drpapineau <vincent.courtois@netsatori.com>
2020-06-22 21:59:14 -0400

    doen with statements

--- Bootcamp Project/02-Python-Statements/task-4.py
+++ Bootcamp Project/02-Python-Statements/task-4.py
@@ -2,8 +2,14 @@
 ###   st = 'Print every word in this sentence that has an even number of letters'
 
 
+st = "Print every word in this sentence that has an even number of letters"
 
-
+for word in st.split():
+    if len(word) % 2 == 0:
+        print (word + " its even")
+        
+        
+        
 
 
 
